bd39ca4993acdd928c47ad360bc47686d0295c7b
Carlos Moraes <carloshrmoraes@gmail.com>
2023-02-08 10:37:24 -0300

    refactor main page to use setlist

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,15 @@
 import styles from './page.module.css';
-import Song from '../components/Song';
 import Metronome from '../components/Metronome';
 import { Quicksand } from '@next/font/google';
-import SongForm from './SongForm';
-import SongController from '../controllers/SongController';
-import { Song as TSong } from '@prisma/client';
+import Setlist from '@/components/Setlist';
 
 const quicksand = Quicksand();
 
 export default async function Home() {
-
-  const songData: TSong[] = await SongController.getInstance().getSongs();
   return (
     <main className={`flex flex-col justify-center items-center h-screen bg-stone-900 ${quicksand.className}`}>
       <Metronome presetRate={120} />
-      {songData.map((s) => {
-        console.log(s);
-        return < Song key={s.id} song={s} />;
-      })}
-      <SongForm />
+      <Setlist />
     </main>
   );
 }
