b522be1751aeb7703327894b98bf2a07ac04e606
Carlos Moraes <carloshrmoraes@gmail.com>
2023-02-15 11:39:29 -0300

    config cleanup

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: false,
-  env: {
-    BASE_FETCH_URL: "http://localhost:3000/",
-  },
   experimental: {
     appDir: true,
     fontLoaders: [
