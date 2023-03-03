49b66ea6f91b3edce28eb180a004a56f36ba434d
Carlos Moraes <carloshrmoraes@gmail.com>
2023-03-03 13:05:35 -0300

    linting

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Setlist from '@/components/Setlist';
 const quicksand = Quicksand();
 
 export default async function Home() {
-
   return (
     <main className={`flex flex-col justify-center items-center h-fit bg-stone-900 ${quicksand.className}`}>
       <Metronome />
