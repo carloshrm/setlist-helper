8e7f44cab78759f817c66de0df2c30d6f05d37c6
Carlos Moraes <carloshrmoraes@gmail.com>
2023-03-03 13:06:01 -0300

    setlist refactor to implement user

--- components/Setlist.tsx
+++ components/Setlist.tsx
@@ -4,7 +4,7 @@ import { Song as SongModel } from '@prisma/client';
 
 import SongView from '@/components/Song';
 import SongForm from '@/components/SongForm';
-import UserBar from './UserBar';
+import User from './User';
 
 export default function Setlist(): ReactElement {
   const [finishedLoading, setFinished] = useState(false);
@@ -12,10 +12,16 @@ export default function Setlist(): ReactElement {
   const [showForm, setShowForm] = useState(false);
   const [localUser, setLocalUser] = useState<string | undefined>(undefined);
 
+  function fetchUser() {
+    return document.cookie.split("; ").find(r => r.startsWith("userid="))?.split("=")[1];
+  }
+
   async function fetchSongs(id: string) {
-    const dbSongs = await fetch("/api/song/getAllSongs", {
+    const dbSongs = await fetch(`${process.env.BASE_URL}/api/song/getAllSongs`, {
       method: 'GET',
-      body: id
+      headers: {
+        cookie: id
+      }
     });
     if (dbSongs.ok) {
       const list = await dbSongs.json() as SongModel[];
@@ -23,27 +29,30 @@ export default function Setlist(): ReactElement {
     }
   }
 
-  async function fetchUser() {
-    const previousSession = document.cookie.split("; ").find(r => r.startsWith("userid="))?.split("=")[1];
-    if (previousSession != undefined) {
-      setLocalUser(previousSession);
-      await fetchSongs(previousSession);
-    }
-  }
-
   useEffect(() => {
     (async () => {
-      await fetchUser();
+      const localID = fetchUser();
+      if (localID != undefined) {
+        setLocalUser(localID);
+        await fetchSongs(localID);
+      }
       setFinished(true);
     })();
   }, []);
 
   useEffect(() => {
-    document.cookie = `userid=${localUser}; SameSite=None; Secure`;
+    if (localUser != undefined)
+      document.cookie = `userid=${localUser}; SameSite=None; Secure`;
   }, [localUser]);
 
-  function saveSetlist() {
-    allSongs.forEach(async s => {
+  useEffect(() => {
+    if (localUser != undefined)
+      saveSetlist(localUser);
+  }, [allSongs]);
+
+  function saveSetlist(id: string) {
+    allSongs.forEach(s => {
+      s.userId = id;
       upsertSong(s);
     });
   }
@@ -57,11 +66,11 @@ export default function Setlist(): ReactElement {
 
   return (
     <div className='bg-stone-800 flex flex-col w-full lg:w-5/6 my-2 p-1 '>
-      <h2>Setlist</h2>
-      {allSongs.length <= 0 ? (<h1 className='m-2'>{finishedLoading ? "Add a song to your setlist." : "Loading..."}</h1>) : allSongs.map((s) => { return <SongView key={s.id} song={s} saveCallback={upsertSong} />; })}
-      <button className='self-start px-2 py-1 border-2 bg-stone-900' onClick={() => setShowForm(s => !s)}>Add</button>
-      {showForm ? <SongForm cclCallback={() => setShowForm(false)} saveCallback={upsertSong} /> : <></>}
-      <UserBar userID={localUser} setUserCallback={setLocalUser} saveCallback={saveSetlist} />
+      <h1 className='text-lg mx-2'>Setlist</h1>
+      {allSongs.length <= 0 ? (<h1 className='m-2'>{finishedLoading ? "Add a song to your setlist." : "Loading..."}</h1>) : allSongs.map((s) => { return <SongView key={s.id} song={s} songStateSetter={setAllSongs} />; })}
+      <button className='self-start px-2 mx-2 border-2 border-black text-stone-900 bg-white' onClick={() => setShowForm(s => !s)}>Add</button>
+      {showForm ? <SongForm cclCallback={() => setShowForm(false)} songStateSetter={setAllSongs} /> : <></>}
+      <User userID={localUser} setUserCallback={setLocalUser} saveCallback={(uid: string) => saveSetlist(uid)} />
     </div>
   );
 }
