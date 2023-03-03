3ec695cb8850c39f380726c35291b1c6357d0070
Carlos Moraes <carloshrmoraes@gmail.com>
2023-03-03 13:08:17 -0300

    changed get endpoint to use id from cookie

--- pages/api/song/getAllSongs.ts
+++ pages/api/song/getAllSongs.ts
@@ -7,9 +7,12 @@ import { Song } from '@prisma/client';
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Song[]>) {
   if (req.method === 'GET') {
     try {
+      const idFromCookie = req.headers.cookie?.split("=")[1];
+
       const info = await prisma.song.findMany({
-        where: { userId: req.body.id }
+        where: { userId: idFromCookie }
       });
+
       return res.status(200).json(info);
     } catch (e) {
       console.log(e);
