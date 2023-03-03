4cf5eb386022ad531ff4bbbe34a6b8c9e40b1d85
Carlos Moraes <carloshrmoraes@gmail.com>
2023-03-03 13:08:30 -0300

    component refactor

--- pages/api/user/deleteUser.ts
+++ pages/api/user/deleteUser.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/prisma/client';
-import { User } from '@prisma/client';
+import { User_info } from '@prisma/client';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<User>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<User_info>) {
   if (req.method === 'POST') {
     const userID: string = JSON.parse(req.body);
     try {
-      const resp = await prisma.user.delete({ where: { id: userID } });
+      const resp = await prisma.user_info.delete({ where: { id: userID } });
       return res.status(200).json(resp);
     } catch (e) {
       return res.status(500);
