65540370f7fffa724731f4eb64f3a19a87e41d46
Carlos Moraes <carloshrmoraes@gmail.com>
2023-03-21 11:27:24 -0300

    footer component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Metronome from '../components/Metronome';
 import { Quicksand } from '@next/font/google';
 import Setlist from '@/components/Setlist';
+import Footer from '@/components/Footer';
 
 const quicksand = Quicksand();
 
 export default async function Home() {
   return (
-    <main className={`flex flex-col justify-center items-center h-fit bg-stone-900 ${quicksand.className}`}>
+    <main className={`flex flex-col items-center h-screen bg-stone-900 ${quicksand.className}`}>
       <Metronome />
       <Setlist />
+      <Footer />
     </main>
   );
 }
